bc6e1cec30930f069e9c443d5875937b6a6204b7
TheTechsTech <technoexpressnet@gmail.com>
2023-03-24 14:25:02 -0400

    build: update libuv_build.bat, windows_run_test-ts.bat and windows_run_test.bat

--- cmd/libuv_build.bat
+++ cmd/libuv_build.bat
@@ -1,17 +1,27 @@
 @echo off
-curl -L https://github.com/libuv/libuv/archive/refs/tags/v%LIBUV_VER%.zip --output libuv-%LIBUV_VER%.zip
-unzip -xoq libuv-%LIBUV_VER%.zip
-cd libuv-%LIBUV_VER%
-cmake .
-msbuild INSTALL.vcxproj /p:Configuration=Release
-mkdir ..\deps\include\uv
-copy /Y include\uv\* ..\deps\include\uv\
-copy /Y include\* ..\deps\include\
-copy /Y Release\uv_a.lib ..\deps\lib\
-copy /Y Release\uv.lib ..\deps\lib\
-copy /Y Release\uv.exp ..\deps\lib\
-copy /Y Release\uv.dll ..\deps\bin\
-cd ..
+if EXIST "C:\Program Files (x86)\Microsoft Visual Studio\2019\Enterprise" (
+	curl -L https://github.com/symplely/libuv/releases/download/libuv-v%LIBUV_VER%-windows/libuv-v%LIBUV_VER%.zip --output libuv-%LIBUV_VER%.zip
+	unzip -xoq libuv-%LIBUV_VER%.zip
+	mkdir deps\include\uv
+	copy /Y libuv-%LIBUV_VER%\bin\* deps\bin\
+	copy /Y libuv-%LIBUV_VER%\include\* deps\include\
+	copy /Y libuv-%LIBUV_VER%\include\uv\* deps\include\uv\
+	copy /Y libuv-%LIBUV_VER%\lib\* deps\lib\
+) else (
+    curl -L https://github.com/libuv/libuv/archive/refs/tags/v%LIBUV_VER%.zip --output libuv-%LIBUV_VER%.zip
+    unzip -xoq libuv-%LIBUV_VER%.zip
+    cd libuv-%LIBUV_VER%
+    cmake .
+    msbuild INSTALL.vcxproj /p:Configuration=Release
+    mkdir ..\deps\include\uv
+    copy /Y include\uv\* ..\deps\include\uv\
+    copy /Y include\* ..\deps\include\
+    copy /Y Release\uv_a.lib ..\deps\lib\
+    copy /Y Release\uv.lib ..\deps\lib\
+    copy /Y Release\uv.exp ..\deps\lib\
+    copy /Y Release\uv.dll ..\deps\bin\
+    cd ..
+)
 
 del libuv-%LIBUV_VER%.zip
 rmdir /S /Q libuv-%LIBUV_VER%
