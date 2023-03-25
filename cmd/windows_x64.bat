4622bbc617fc4fe9c2c5c1d6721d73285185ce10
TheTechsTech <technoexpressnet@gmail.com>
2023-03-25 11:53:05 -0400

    build: update windows_x64.bat

--- cmd/windows_x64.bat
+++ cmd/windows_x64.bat
@@ -9,7 +9,7 @@ if "%PHP_VER%" == "7.4.33" (
 )
 
 IF NOT EXIST php-sdk (
-  curl -L https://github.com/microsoft/php-sdk-binary-tools/archive/refs/tags/php-sdk-2.2.0.tar.gz | tar xzf - && ren php-sdk-binary-tools-php-sdk-2.2.0 php-sdk
+  curl -L https://github.com/php/php-sdk-binary-tools/archive/refs/tags/php-sdk-2.2.0.tar.gz | tar xzf - && ren php-sdk-binary-tools-php-sdk-2.2.0 php-sdk
 )
 
 IF NOT EXIST php-sdk\phpdev\%CRT%\x64 (mkdir php-sdk\phpdev\%CRT%\x64)
@@ -34,7 +34,7 @@ set ARCH=x64
 if not defined PHP_TS set ZTS=--disable-zts
 copy /Y ..\cmd\phpsdk_setshell.bat bin\phpsdk_setshell.bat
 bin\phpsdk_setshell.bat %CRT% x64 && bin\phpsdk_setvars.bat && bin\phpsdk_dumpenv.bat && bin\phpsdk_buildtree.bat phpdev && cd php-%PHP_VER% && IF NOT EXIST config.nice (..\..\..\..\bin\phpsdk_deps -u --no-backup) && IF NOT EXIST "..\deps\include\uv" (
-  cd .. && ..\..\..\..\cmd\libuv_build.bat && cd php-%PHP_VER% && buildconf --add-modules-dir=..\pecl\ && configure --enable-cli %UV_SHARED% %ZTS% --enable-sockets && nmake snap && cd ..\..\..\..\.. && if EXIST config.w32.bak ( ren config.w32 config.w32.shared && ren config.w32.bak config.w32)
+  cd .. && ..\..\..\..\cmd\libuv_build.bat && cd php-%PHP_VER% && buildconf --add-modules-dir=..\pecl\ && configure --enable-cli %UV_SHARED% %ZTS% --enable-sockets --with-ffi && nmake snap && cd ..\..\..\..\.. && if EXIST config.w32.bak ( ren config.w32 config.w32.shared && ren config.w32.bak config.w32)
 ) else (
-  buildconf --force --add-modules-dir=..\pecl\ && configure --enable-cli %UV_SHARED% %ZTS% --enable-sockets && nmake snap && cd ..\..\..\..\.. && if EXIST config.w32.bak ( ren config.w32 config.w32.shared && ren config.w32.bak config.w32)
+  buildconf --force --add-modules-dir=..\pecl\ && configure --enable-cli %UV_SHARED% %ZTS% --enable-sockets --with-ffi && nmake snap && cd ..\..\..\..\.. && if EXIST config.w32.bak ( ren config.w32 config.w32.shared && ren config.w32.bak config.w32)
 )
