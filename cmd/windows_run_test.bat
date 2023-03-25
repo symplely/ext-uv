c3162c27feffcba4e870b980f84f60895b79c235
Lawrence Stubbs <lstubbs@techno.express>
2023-03-25 06:00:57 -0400

    build: update windows_run_test.bat

--- cmd/windows_run_test.bat
+++ cmd/windows_run_test.bat
@@ -1,5 +1,5 @@
 @echo off
-if not defined PHP_VER set PHP_VER=8.2.4
+call cmd\getopt.bat %*
 
 if "%PHP_VER%" == "7.4.33" (
     set CRT=vc15
@@ -7,7 +7,6 @@ if "%PHP_VER%" == "7.4.33" (
     set CRT=vs16
 )
 
-call cmd\getopt.bat %*
 dir php-sdk\phpdev\%CRT%\x64\php-%PHP_VER%\x64\Release%PHP_TS%
 dir php-sdk\phpdev\%CRT%\x64\php-%PHP_VER%\x64\Release%PHP_TS%\php-%PHP_VER%
 dir php-sdk\phpdev\%CRT%\x64\php-%PHP_VER%\x64\Release%PHP_TS%\pecl\uv
