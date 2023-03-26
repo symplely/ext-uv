e0e4534ed803e96101ed01d4ee86c66d5facc54a
Lawrence Stubbs <lstubbs@techno.express>
2023-03-26 05:19:22 -0400

    build: update libuv_build.bat

--- cmd/libuv_build.bat
+++ cmd/libuv_build.bat
@@ -3,10 +3,8 @@
     curl -L https://github.com/libuv/libuv/archive/refs/tags/v%LIBUV_VER%.zip --output libuv-%LIBUV_VER%.zip
     unzip -xoq libuv-%LIBUV_VER%.zip
     cd libuv-%LIBUV_VER%
-    cmake . -DBUILD_TESTING=OFF -DCMAKE_BUILD_TYPE=Release
+    cmake . -DBUILD_TESTING=OFF
     cmake --build . --config Release
-    rem cmake . -DBUILD_TESTING=OFF
-    rem msbuild INSTALL.vcxproj /p:Configuration=Release
     mkdir ..\deps\include\uv
     copy /Y include\uv\* ..\deps\include\uv\
     copy /Y include\* ..\deps\include\
@@ -21,6 +19,3 @@
 
 del libuv-%LIBUV_VER%.zip
 rmdir /S /Q libuv-%LIBUV_VER%
-rem copy /Y "C:\Program Files (x86)\Windows Kits\10\Lib\10.0.19041.0\um\x64\UserEnv.Lib" deps\lib\
-rem copy /Y "C:\Program Files (x86)\Windows Kits\10\Include\10.0.19041.0\um\UserEnv.h" deps\include\
-rem copy /Y C:\Windows\SysWOW64\userenv.dll deps\bin\
