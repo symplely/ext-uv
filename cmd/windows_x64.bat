3bc967dc42b3afa3f2d97a893e36e60d5bc9304b
TheTechsTech <technoexpressnet@gmail.com>
2023-03-27 16:21:04 -0400

    updates/corrections, and rename CI files

--- cmd/windows_x64.bat
+++ cmd/windows_x64.bat
@@ -34,7 +34,7 @@ set ARCH=x64
 if not defined PHP_TS set ZTS=--disable-zts
 copy /Y ..\cmd\phpsdk_setshell.bat bin\phpsdk_setshell.bat
 bin\phpsdk_setshell.bat %CRT% x64 && bin\phpsdk_setvars.bat && bin\phpsdk_dumpenv.bat && bin\phpsdk_buildtree.bat phpdev && cd php-%PHP_VER% && IF NOT EXIST config.nice (..\..\..\..\bin\phpsdk_deps -u --no-backup) && IF NOT EXIST "..\deps\include\uv" (
-  cd .. && ..\..\..\..\cmd\libuv_build.bat && cd php-%PHP_VER% && buildconf --add-modules-dir=..\pecl\ && configure --enable-cli %UV_SHARED% %ZTS% --enable-sockets --with-ffi && nmake snap && cd ..\..\..\..\.. && if EXIST config.w32.bak ( ren config.w32 config.w32.shared && ren config.w32.bak config.w32)
+  cd .. && ..\..\..\..\cmd\libuv_build.bat && cd php-%PHP_VER% && buildconf --add-modules-dir=..\pecl\ && configure --enable-cli %UV_SHARED% %ZTS% --enable-sockets --with-ffi --with-openssl --enable-mbstring --with-pdo-pgsql && nmake snap && cd ..\..\..\..\.. && if EXIST config.w32.bak ( ren config.w32 config.w32.shared && ren config.w32.bak config.w32)
 ) else (
-  buildconf --force --add-modules-dir=..\pecl\ && configure --enable-cli %UV_SHARED% %ZTS% --enable-sockets --with-ffi && nmake snap && cd ..\..\..\..\.. && if EXIST config.w32.bak ( ren config.w32 config.w32.shared && ren config.w32.bak config.w32)
+  buildconf --force --add-modules-dir=..\pecl\ && configure --enable-cli %UV_SHARED% %ZTS% --enable-sockets --with-ffi --with-openssl --enable-mbstring --with-pdo-pgsql && nmake snap && cd ..\..\..\..\.. && if EXIST config.w32.bak ( ren config.w32 config.w32.shared && ren config.w32.bak config.w32)
 )
